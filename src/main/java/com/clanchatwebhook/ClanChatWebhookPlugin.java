eaa4eeb0ad53d12c43fb794b821e43af3c685e60
Joshua Blackman <joshuadennisblackman@gmail.com>
2023-09-02 12:14:08 +0100

    remove unused import

--- src/main/java/com/clanchatwebhook/ClanChatWebhookPlugin.java
+++ src/main/java/com/clanchatwebhook/ClanChatWebhookPlugin.java
@@ -4,7 +4,6 @@ import com.google.inject.Provides;
 import javax.inject.Inject;
 
 import com.google.common.base.Strings;
-import jdk.internal.org.jline.utils.Log;
 import lombok.extern.slf4j.Slf4j;
 import net.runelite.api.ChatMessageType;
 import net.runelite.api.Client;
